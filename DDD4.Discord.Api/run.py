f359f5f18d511ca1c0473328d2b70e8a0f6f9f12
123qq1 <jeppe.bjerremand@gmail.com>
2023-04-19 16:13:48 +0200

    Added Discord bot to docker compose

---
+++ DDD4.Discord.Api/run.py
@@ -0,0 +1,4 @@
+import test_bot
+
+if __name__ == '__main__':
+    test_bot.run_discord_bot();
---
+++ DDD4.Discord.Api/test_bot.py
@@ -0,0 +1,86 @@
+import discord
+from tabulate import tabulate
+
+dict = {'🔼':1,'🔽':-1,'⏫':2,'⏬':-2}
+
+current_standing = {}
+
+
+def handle_response(message: str, standings) -> str:
+    p_message = message.lower()
+
+    if p_message == 'test':
+        return 'TEST'
+
+    if p_message == 'standing':
+
+        result = ""
+
+        items = standings.items()
+
+        col_names = ['User','Points']
+
+        return tabulate(items, col_names,tablefmt="github")
+
+
+    return 'Not a known command'
+
+
+async def send_message(message, user_message, is_private):
+    try:
+        response = handle_response(user_message,current_standing)
+        await message.author.send(response) if is_private else await message.channel.send(response)
+
+    except Exception as e:
+        print(e)
+
+def run_discord_bot():
+    TOKEN = 'OTUzNjA1Mjg0NTIwMTU3MjE0.YjHAIA.znV3teG6fzXQVWb-qg77X0nzOXY'
+
+    intents = discord.Intents.default()
+    intents.message_content = True
+    intents.reactions = True
+
+    client = discord.Client(intents = intents)
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+
+        username = str(message.author)
+        raw_message = str(message.content)
+        message_type = raw_message[0]
+        user_message = raw_message[1:]
+        channel = str(message.channel)
+
+        if message_type == '?':
+            await send_message(message, user_message, is_private=True)
+        elif message_type == '!':
+            await send_message(message,user_message, is_private=False)
+
+    @client.event
+    async def on_reaction_add(reaction,user):
+        if user == client.user:
+            return
+
+        user_message = str(reaction.emoji)
+
+        if user_message not in dict.keys():
+            return
+        change = dict[user_message]
+
+        user_name = str(reaction.message.author)
+
+        if user_name not in current_standing.keys():
+            current_standing[user_name] = change
+        else:
+            current_standing[user_name] += change
+
+
+
+    client.run(TOKEN)
