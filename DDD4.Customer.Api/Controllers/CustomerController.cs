6229719628152dec59a2c1f94dca62b7726640cc
GSittrup <94045713+GSittrup@users.noreply.github.com>
2023-04-19 14:44:35 +0200

    dunno man

---
+++ DDD4.Customer.Api/Controllers/CustomerController.cs
@@ -0,0 +1,37 @@
+﻿using DDD4.Customer.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using DDD4.Customer.Application.CQRS.Commands;
+
+namespace DDD4.Customer.Api.Controllers
+{
+
+    [Route("/api/Customer")]
+    public class CustomerController : ControllerBase
+    {
+        private CustomerRepository _customerRepository;
+        private readonly ICustomerCreateCommand _customerCreateCommand;
+
+        public CustomerController(CustomerRepository customerRepository, ICustomerCreateCommand customerCreateCommand)
+        {
+            _customerRepository = customerRepository;
+            _customerCreateCommand = customerCreateCommand;
+        }
+
+        [HttpPost]
+        [Route("create")]
+        public async Task<IActionResult> Create(CustomerCreateRequestDto dto)
+        {
+            var customer = await _customerRepository.LoadAsync(dto.CustomerId);
+            customer.Create(
+                dto.CustomerId,
+                dto.CustomerName,
+                dto.DiscordName,
+                dto.AccountName
+                );
+
+            await _customerRepository.SaveAsync(customer);
+
+            return Ok(); 
+        }
+    }
+}
---
+++ DDD4.Customer.Application/CQRS/Commands/ICustomerCreateCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD4.Customer.Application.CQRS.Commands
+{
+    public interface ICustomerCreateCommand
+    {
+        void Create(CustomerCreateRequestDto customerCreateRequestDto);
+    }
+}
