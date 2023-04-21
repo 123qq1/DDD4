770171e8804e934aa6fb6f1ed0c8015a6f2c77a0
GSittrup <94045713+GSittrup@users.noreply.github.com>
2023-04-21 10:02:09 +0200

    Aggregate on Order. Need a merge before moving on.

--- DDD4.Ordre.Domain/Framework/Aggregate.cs
+++ DDD4.Ordre.Domain/Framework/Aggregate.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace DDD4.Order.Domain.Framework
 {
-    public class Aggregate
+    public abstract class Aggregate
     {
+        readonly IList<object> _changes = new List<object>();
+
+        public Guid Guid { get; protected set; } = Guid.Empty;
+        public long Version { get; private set; } = -1;
+
+        protected abstract void When(object @event);
+
+        public void Apply(object @event)
+        {
+            When(@event);
+            _changes.Add(@event);
+        }
+
+        public void Load(long version, IEnumerable<object> history)
+        {
+            Version = version;
+
+            foreach (var item in history)
+            {
+                When(item);
+            }
+        }
+
+        public object[] GetChanges() => _changes.ToArray();
     }
 }
