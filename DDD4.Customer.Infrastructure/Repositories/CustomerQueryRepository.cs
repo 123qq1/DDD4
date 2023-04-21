dc109d61218a0422cb9c20f6027817884dc449d5
GSittrup <94045713+GSittrup@users.noreply.github.com>
2023-04-21 09:58:03 +0200

    mongoDb customerQuery Add metode

--- DDD4.Customer.Infrastructure/Repositories/CustomerQueryRepository.cs
+++ DDD4.Customer.Infrastructure/Repositories/CustomerQueryRepository.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Bson;
+//using MongoDB.Driver;
 
 namespace DDD4.Customer.Infrastructure.Repositories
 {
@@ -12,7 +14,21 @@ namespace DDD4.Customer.Infrastructure.Repositories
         // mongo db 
         async Task ICustomerQueryRepository.Add(Domain.Entities.Customer customer)
         {
-            throw new NotImplementedException();
+
+            //MongoClient mongoClient = new MongoClient("connstring");
+
+            var db = mongoClient.GetDatabase("CustomersDocuments");
+            var collection = db.GetCollection<BsonDocument>();
+
+            var documentCustomer = new BsonDocument
+            {
+                { "Id", customer.Id },
+                { "Name", customer.Name },
+                { "DiscordName", customer.DiscordName },
+                { "AccountName", customer.AccountName },
+            };
+
+            await collection.InsertOneAsync(documentCustomer);
         }
     }
 }
