68158794aea57a48b15ca280a4d7ede4011fb553
GSittrup <94045713+GSittrup@users.noreply.github.com>
2023-04-25 17:14:04 +0200

    comments

--- DDD4.Ordre.Application/CQRS/Commands/IOrderCreateCommand.cs
+++ DDD4.Ordre.Application/CQRS/Commands/IOrderCreateCommand.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DDD4.Order.Application.CQRS.Commands.CreateCustomer; //ødelagt namespace (y)
 
 namespace DDD4.Order.Application.CQRS.Commands
 {
     public interface IOrderCreateCommand
     {
-        Task Create(CreateOrderCommand createOrderCommand);
+        Task Create(CreateOrderCommand createOrderCommand); // skal den overhovedet være her???
     }
 }
