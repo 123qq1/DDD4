7f52025359104ce2e9083abc7d1a20388bc7249a
123qq1 <jeppe.bjerremand@gmail.com>
2023-05-01 18:49:08 +0200

    Pika now runs Asnyc, still needs message handling

--- DDD4.Discord.Api/run.py
+++ DDD4.Discord.Api/run.py
@@ -2,20 +2,20 @@ import test_bot
 import test_queue
 import asyncio
 
-def start(c, t):
+async def start(c,t):
     await c.start(t)
 
 if __name__ == '__main__':
     db = test_bot.bot()
 
     (client, token) = db.run_discord_bot()
-
     print("test")
     rb = test_queue.rabbit(client)
     rb.CheckService()
-    print("test")
-    rb.connect()
-    asyncio.run(start(client,token))
+    print("connected")
+    rb.run()
+    #client.run(token)
+    #asyncio.create_task(start(client,token))
 
 
 
--- DDD4.Discord.Api/test_queue.py
+++ DDD4.Discord.Api/test_queue.py
@@ -6,23 +6,62 @@ class rabbit:
 
     def __init__(self, client):
         self.client = client
+        self.credentials = pika.PlainCredentials('guest','guest')
+        self.props = {'connection_name': 'DDD4.Discord.Api'}
 
-    def connect(self):
-        credentials = pika.PlainCredentials('guest','guest')
+        self.parameters = pika.ConnectionParameters('rabbitmq',virtual_host='/',
+                                                    credentials=self.credentials, heartbeat=50,
+                                                    client_properties=self.props)
 
-        parameters = pika.ConnectionParameters('rabbitmq',virtual_host='/',credentials=credentials)
+        self.connection = pika.SelectConnection(self.parameters,self.on_connected)
 
-        connection = pika.BlockingConnection(parameters)
+    def on_connected(self, connection):
+        """
+        Called by pika when a connection is established.
+        """
+        print("channel")
+        self.connection.channel(on_open_callback=self.on_channel_open)
 
-        channel = connection.channel()
-        channel.basic_consume(queue="LinkCustomer", auto_ack=True, on_message_callback=self.reciveMessage)
-        print(f'RabbitMQ is now running')
+    def on_channel_open(self, channel):
+        """
+        Called by pika when the channel is opened.
+        """
+        print("consuming")
+        self.channel = channel
+        self.channel.basic_qos(prefetch_count=10)
+        self.channel.add_on_close_callback(self.on_channel_closed)
+        self.start_consuming()
+
+    def on_channel_closed(self, channel, reply_code, reply_text):
+        """
+        Called by pika when the channel is closed.
+        """
+        print(reply_text)
+
+        self.connection.close()
+
+    def on_consumer_cancelled(self, frame):
+        """
+        Called by pika when the RabbitMQ connection is lost.
+        """
+        print("Cancelled")
+        self.channel.close()
+
+    def start_consuming(self):
+        """
+        Start consuming messages.
+        """
+        self.channel.add_on_cancel_callback(self.on_consumer_cancelled)
+        self.consumer_tag = self.channel.basic_consume(queue="LinkCustomer",
+                                                       auto_ack=True, on_message_callback=self.reciveMessage)
+
 
     def reciveMessage(self, ch, method, properties, body):
         print(body)
-        user = self.client.get_user("123qq1#4884")
-        user.send(body)
 
+        print(type(body))
+        #user = self.client.get_user("123qq1#4884")
+        #user.send(body)
 
     def CheckService(self):
         pingcounter = 0
@@ -33,6 +72,9 @@ class rabbit:
                 s.connect(('rabbitmq', 5672))
                 isreachable = True
             except socket.error as e:
-                time.sleep(3)
+                time.sleep(15)
                 pingcounter += 1
             s.close()
+
+    def run(self):
+        self.connection.ioloop.start()
